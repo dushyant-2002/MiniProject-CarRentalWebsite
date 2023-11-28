c938fd401661bef5af953add74469ee20e26a549
dushyant-2002 <100183850+dushyant-2002@users.noreply.github.com>
2023-11-28 12:58:29 +0530

    FinalCommitv3

--- router/authentication.js
+++ router/authentication.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const User = require('../models/User');
+const auth = require("../middlewares/auth");
+const mongoose = require('mongoose');
 const { signup, signin } = require('../controllers/userController');
 const router = express.Router();
 
@@ -7,6 +9,17 @@ const router = express.Router();
 router.post('/signup',signup);
 
 router.post('/signin',signin);
+
+router.get("/login",async(req,res)=>{
+    res.render("user/login");
+})
+router.get("/signup",async(req,res)=>{
+    res.render("user/signup");
+})
+router.get("/logout",async(req,res)=>{
+    await res.clearCookie('access_token');
+    res.redirect("/book");
+})
 
 
 module.exports = router;
